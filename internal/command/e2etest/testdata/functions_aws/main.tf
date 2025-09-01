9191271742a5f3c9b29253366675e24a6002370e
Maksym Nazarenko <maks.nazarenko@gmail.com>
2025-09-01 20:59:38 +0300

    use gcp functions

--- internal/command/e2etest/testdata/functions_aws/main.tf
+++ internal/command/e2etest/testdata/functions_aws/main.tf
@@ -1,17 +1,22 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.68.0"
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = "6.11.0"
+    # }
+    google = {
+      source = "hashicorp/google"
     }
   }
 }
 
 variable "bucket_name" {
   type    = string
-  default = "bucket-prod"
+  # default = "bucket-prod"
+  default = "us-central1-a"
 }
 
 output "stuff" {
-  value = provider::aws::arn_build("aws", "s3", "", "", var.bucket_name)
+  # value = provider::aws::arn_build("aws", "s3", "", "", var.bucket_name)
+  value = provider::google::region_from_zone(var.bucket_name)
 }
