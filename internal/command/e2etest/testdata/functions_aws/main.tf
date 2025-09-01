e643a2ca4499e3be0f9df04958b944dc7be8fc91
Maksym Nazarenko <maks.nazarenko@gmail.com>
2025-09-01 20:34:18 +0300

    use aws provider

--- internal/command/e2etest/testdata/functions_aws/main.tf
+++ internal/command/e2etest/testdata/functions_aws/main.tf
@@ -1,22 +1,17 @@
 terraform {
   required_providers {
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "6.11.0"
-    # }
-    google = {
-      source = "hashicorp/google"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.7.0"
     }
   }
 }
 
 variable "bucket_name" {
   type    = string
-  # default = "bucket-prod"
-  default = "us-central1-a"
+  default = "bucket-prod"
 }
 
 output "stuff" {
-  # value = provider::aws::arn_build("aws", "s3", "", "", var.bucket_name)
-  value = provider::google::region_from_zone(var.bucket_name)
+  value = provider::aws::arn_build("aws", "s3", "", "", var.bucket_name)
 }
